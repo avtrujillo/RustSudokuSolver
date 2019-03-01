c6aeeab4c5c247e4771a1211eb5f870851dee6e9
avtrujillo <alexandervtrujillo@gmail.com>
2019-02-28 17:39:16 -0800

    Now only get warnings instead of errors

--- src/main.rs
+++ src/main.rs
@@ -8,62 +8,47 @@ fn main() {
     println!("Hello, world!");
 }
 
+#[derive(Clone, Copy, Debug)]
 enum SudokuDigit {
-    Known(u8),
+    Known(u32),
     Unknown
 }
 
 impl SudokuDigit {
 
-    fn from_chars(chars: Chars) -> FilterMap<Chars, SudokuDigit> {
-        let mut fm: FilterMap<Chars, SudokuDigit> = chars.filter_map( |c|
-            match c {
-                '_' => Some(SudokuDigit::Unknown),
-                _ => if let d = c.to_digit() {
-                    Some(SudokuDigit::Known(d as u8))
-                }
-                else {
-                    None
-                }
-            }
-        );
-        array_from_filtermap(&mut fm)
+    fn get_puzzle_input() -> [SudokuDigit; 89] {
+
+        let mut input_string = String::new();
+        let mut sudoku_file = File::open("sudoku_input.txt").expect("Unable to read file");
+        sudoku_file.read_to_string(&mut input_string);
+        SudokuDigit::from_chars(input_string.as_str().chars())
+
     }
 
-    fn array_from_filtermap(&mut fm: FilterMap<Chars, SudokuDigit>) -> [u8; 89] {
+    fn from_chars(chars: Chars) -> [SudokuDigit; 89] {
+        let mut fm = chars.filter_map(|c| SudokuDigit::digit_match(c));
 
-        let mut digit_array = [0; 89];
+        let mut digit_array = [SudokuDigit::Unknown; 89];
 
-        for i in (0..88) {
-            digit_array[i] = fm.next();
+        for i in 0..88 {
+            let next_digit = match fm.next() {
+                Some(digit) => digit,
+                None => panic!("Incorrect number of digits read from file")
+            };
+            digit_array[i] = next_digit;
         }
 
         digit_array
     }
 
-    fn get_puzzle_input() -> [u32; 89] {
-
-        let mut input_string = String::new();
-        let mut sudoku_file = File::open("sudoku_input.txt").expect("Unable to read file");
-        sudoku_file.read_to_string(&mut input_string);
-        from_chars(input_string.as_str().chars())
-
+    fn digit_match(input_char: char) -> Option<SudokuDigit> {
+        match input_char {
+            '_' => Some(SudokuDigit::Unknown),
+            _ => match input_char.to_digit(10) {
+                Some(digit) => Some(SudokuDigit::Known(digit)),
+                None => None
+            }
+        }
     }
 
 }
-
-
-/*
-fn get_digit_array_from_iter(&mut digit_iter: FilterMap<Chars, u8>) -> [u8; 89] {
-    let mut digit_array: [u8; 89] = [0; 89];
-    for i in 0..88 {
-        if let Some(digit) = digit_iter.next() {
-            digit_array[i] = digit;
-        }
-        else {
-            panic!("Something went wrong in processing the input")
-        }
-    }
-    digit_array
-}
-*/

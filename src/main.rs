6c74fcaedf7c054a37302e09e6ca8c5550b7cafb
avtrujillo <alexandervtrujillo@gmail.com>
2019-03-01 12:40:12 -0800

    moved SudokuBoard class into its own module

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-mod sudoku_digit;
-use sudoku_digit::SudokuDigit;
+mod sudoku_board;
+use sudoku_board::SudokuBoard as SudokuBoard;
+use sudoku_board::SudokuDigit as SudokuDigit;
 
 use std::fs::*;
 use std::iter::*;
@@ -14,29 +15,7 @@ fn main() {
     println!("{:?}", sd_board);
 }
 
-struct SudokuBoard {
-    tiles: [SudokuDigit; 81],
-}
 
-impl SudokuBoard {
-
-    fn new(sd_array: [SudokuDigit; 81]) ->  SudokuBoard {
-        SudokuBoard {tiles: sd_array}
-    }
-}
-
-impl fmt::Debug for SudokuBoard {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut debug_string = String::new();
-        for (i, digit) in self.tiles.into_iter().enumerate() {
-            debug_string.push_str(digit.debug_output().as_str());
-            if (i + 1) % 9 == 0 {
-                debug_string.push_str("\n");
-            }
-        };
-        write!(f, "{}", debug_string.as_str())
-    }
-}
 
 
 
---
+++ src/sudoku_board/mod.rs
@@ -0,0 +1,33 @@
+#[path = "../sudoku_digit/mod.rs"]
+pub mod sudoku_digit;
+pub use sudoku_digit::SudokuDigit as SudokuDigit;
+
+use std::fs::*;
+use std::iter::*;
+use std::str::Chars;
+use std::io::Read;
+use std::fmt;
+
+pub struct SudokuBoard {
+    tiles: [SudokuDigit; 81],
+}
+
+impl SudokuBoard {
+
+    pub fn new(sd_array: [SudokuDigit; 81]) ->  SudokuBoard {
+        SudokuBoard {tiles: sd_array}
+    }
+}
+
+impl fmt::Debug for SudokuBoard {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut debug_string = String::new();
+        for (i, digit) in self.tiles.into_iter().enumerate() {
+            debug_string.push_str(digit.debug_output().as_str());
+            if (i + 1) % 9 == 0 {
+                debug_string.push_str("\n");
+            }
+        };
+        write!(f, "{}", debug_string.as_str())
+    }
+}

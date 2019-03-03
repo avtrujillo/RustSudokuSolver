5df0d5151427b2605d99389e785a2054db892d15
avtrujillo <alexandervtrujillo@gmail.com>
2019-03-02 16:54:54 -0800

    added directory and file for guess_branch module

---
+++ src/guess_branch/mod.rs
@@ -0,0 +1,10 @@
+#[path = "../sudoku_board/mod.rs"]
+pub mod sudoku_board;
+
+use sudoku_board::SudokuBoard as SudokuBoard;
+use sudoku_board::SudokuDigit as SudokuDigit;
+
+
+pub struct GuessBranch {
+
+}

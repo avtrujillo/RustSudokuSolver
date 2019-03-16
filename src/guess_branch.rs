3bb3aca194b3cbc76c09156594e7e4545f5d3f2c
avtrujillo <alexandervtrujillo@gmail.com>
2019-03-15 22:39:09 -0700

    Refactored make_guesses()

--- src/guess_branch.rs
+++ src/guess_branch.rs
@@ -6,6 +6,7 @@ use std::fmt;
 use std::iter::Map;
 use std::vec::*;
 use std::slice::Iter;
+use std::collections::HashMap;
 use crate::sudoku_digit::SudokuDigit::Guess;
 use crate::guess_branch::BranchResult::InProgress;
 
@@ -42,14 +43,16 @@ impl GuessBranch {
 
         loop {
             match branch_result {
-                BranchResult::Solved | BranchResult::NoSolution => {break branch_result},
-                BranchResult::GuessNeeded => {branch_result = self.make_guesses()},
+                BranchResult::Solved(_) | BranchResult::NoSolution => {break branch_result},
+                BranchResult::GuessNeeded => {
+                    branch_result = self.make_guesses()
+                },
                 BranchResult::Deduced(preexisting_vec) => {
                     for deduced in preexisting_vec {self.set_deduced(deduced.0, deduced.1)};
                     branch_result = InProgress
                 },
                 BranchResult::InProgress => {
-                    self.run_ninesets();
+                    branch_result = self.run_ninesets()
                 }
             }
         }
@@ -60,18 +63,18 @@ impl GuessBranch {
         let ninesets_map: Vec<BranchResult> = self.ninesets.iter_mut().map( |ns|
             ns.remove_knowns_and_guesses(&board_clone)
         ).collect();
-        Self::process_ninesets_results(ninesets_map)
+        (Self::process_ninesets_results(ninesets_map), self.board.clone())
     }
 
     fn process_ninesets_results(ns_brs: Vec<BranchResult>) -> BranchResult {
-        let mut overall_result = BranchResult::Solved;
+        let mut overall_result = BranchResult::Solved(SudokuBoard::new([SudokuDigit::Unknown; 81]));
         for br in ns_brs {
             match br {
                 BranchResult::NoSolution => {return br;}, // if any one nineset has no solution, then the overall puzzle has no solution
                 BranchResult::Deduced(deduced_vec) => {
                     match overall_result.clone() {
                         BranchResult::Deduced(mut preexisting_vec) => {preexisting_vec.extend(deduced_vec);},
-                        BranchResult::InProgress | BranchResult::Solved | BranchResult::GuessNeeded => {
+                        BranchResult::InProgress | BranchResult::Solved(_) | BranchResult::GuessNeeded => {
                             overall_result = BranchResult::Deduced(deduced_vec);
                         }
                         BranchResult::NoSolution => {panic!("Should be impossible")}
@@ -80,7 +83,7 @@ impl GuessBranch {
                 BranchResult::InProgress => {
                     match overall_result {
                         BranchResult::Deduced(_) | BranchResult::InProgress => (),
-                        BranchResult::GuessNeeded | BranchResult::Solved => {overall_result = BranchResult::InProgress;}
+                        BranchResult::GuessNeeded | BranchResult::Solved(_) => {overall_result = BranchResult::InProgress;}
                         BranchResult::NoSolution => {panic!("Should be impossible")}
                     };
 
@@ -89,11 +92,11 @@ impl GuessBranch {
                     match overall_result {
                         BranchResult::NoSolution | BranchResult::Deduced(_) | BranchResult::InProgress =>
                             {panic!("Should be impossible")},
-                        BranchResult::Solved => {overall_result = BranchResult::GuessNeeded;},
-                        BranchResult::GuessNeeded | BranchResult::Solved | BranchResult::NoSolution => (),
+                        BranchResult::Solved(_) => {overall_result = BranchResult::GuessNeeded;},
+                        BranchResult::GuessNeeded | BranchResult::NoSolution => (),
                     }
                 },
-                BranchResult::Solved => ()
+                BranchResult::Solved(solution) => {overall_result = BranchResult::Solved(solution);}
             };
             ();
         };
@@ -104,9 +107,32 @@ impl GuessBranch {
         self.board.tiles()[digit_coors.to_index()] = SudokuDigit::Known(digit_value);
     }
 
-    fn make_guesses(&self) -> BranchResult { //TODO
-        let branch_result = BranchResult::InProgress;
-        branch_result
+    fn make_guesses(&self) -> BranchResult {
+        let branches = self.create_guess_branches();
+        let results: Vec<(BranchResult, Board)> = branches.map(|branch|branch.run_ninesets());
+        let (d, ip, gn, s, ns) = BranchResult::sort_results(results.iter());
+        if d.len() == 1 {
+            d[0]
+        }
+        else {
+            BranchResult::NoSolution // Returns "NoSolution" when there are multiple solutions as well as when there are zero
+            // Not sure whether this behavior is desirable
+        }
+    }
+
+    fn create_guess_branches(&self) -> Vec<GuessBranch> {
+        self.board.tiles().filter(|tile| tile == SudokuDigit::Unkown).enumerate().map( |(ind, uk)|
+                self.ninesets.filter_map(|ns| ns.tile_coors.any( |coor|
+                    match (coor == DigitCoors::from_index(ind as usize)) {
+                        false => None,
+                        true => Some(ns.possibilities().map( |poss|
+                                GuessBranch::new(ind, poss, &self.board.clone())
+                            )
+                        ),
+                    }
+                )
+            ).flatten()
+        ).collect()
     }
 
 }
@@ -115,8 +141,25 @@ pub enum BranchResult {
     Deduced(Vec<(u32, DigitCoors)>),
     InProgress,
     GuessNeeded,
-    Solved,
+    Solved(SudokuBoard),
     NoSolution
+}
+
+impl BranchResult {
+    fn sort_results(results: impl Iterator) -> [Vec<BranchResult>; 5] {
+        let my_vec: Vec<BranchResult> = vec![];
+        let (d, ip, gn, s, ns) = (my_vec.copy(), my_vec.copy(), my_vec.copy(), my_vec.copy(), my_vec.copy());
+        for result in results {
+            match result {
+                BranchResult::Deduced(_) => d,
+                BranchResult::InProgress => ip,
+                BranchResult::GuessNeeded => gn,
+                BranchResult::Solved(_) => s,
+                BranchResult::NoSolution => ns
+            }.push(result);
+        };
+        [d, ip, gn, s, ns]
+    }
 }
 
 
